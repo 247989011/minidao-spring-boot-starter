e2d60a890a87fc21c5e0a9cc93594923683a66ae
panqq <pan247989011@163.com>
2018-09-16 23:13:44 +0800

    Initial commit

---
+++ src/main/java/top/pqq/MinidaoSpringBootStarterApplication.java
@@ -0,0 +1,12 @@
+package top.pqq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinidaoSpringBootStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinidaoSpringBootStarterApplication.class, args);
+    }
+}
